aa07dd7e485fb3b5afab0c00ec5e1486283a96b9
Brad Trehan <bht822@usask.ca>
2022-05-29 23:24:49 -0600

    Added the save button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -6,20 +8,19 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final word = WordPair.random();
-
     return MaterialApp(
       title: 'Startup Name Generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Name Generator'),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
         ),
-        body: const Center(child: RandomWords()),
       ),
+      home: const RandomWords(),
     );
   }
 }
@@ -34,24 +35,81 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestion = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
-  final _likedWords = <WordPair> {};  //Dict to stored the liked words
+  final _likedWords = <WordPair>{}; //Dict to stored the liked words
   @override
   final wordPair = WordPair.random();
   Widget build(BuildContext context) {
-    return ListView.builder(padding: const EdgeInsets.all(16.0),
-    itemBuilder: (context,i){
-      if(i.isOdd) return const Divider();
-      final index = i~/2;
-      if(index >= _suggestion.length){
-        _suggestion.addAll(generateWordPairs().take(10));
-      }
-      return ListTile(
-        title: Text(
-          _suggestion[index].asPascalCase,
-          style: _biggerFont,
-        ),
-      );
-    },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            onPressed: _pushSaved,
+            icon: const Icon(Icons.list),
+            tooltip: 'Save Suggestions',
+          )
+        ],
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestion.length) {
+            _suggestion.addAll(generateWordPairs().take(10));
+          }
+          final _alreadyLiked = _likedWords.contains(_suggestion[
+              index]); //Check if the name is saved already , if not show heart icon
+          return ListTile(
+            title: Text(
+              _suggestion[index].asPascalCase,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              _alreadyLiked ? Icons.favorite : Icons.favorite_border,
+              color: _alreadyLiked ? Colors.red : null,
+              semanticLabel: _alreadyLiked ? 'Remove from saved' : 'Save',
+            ),
+            onTap: () {
+              setState(() {
+                if (_alreadyLiked) {
+                  _likedWords.remove(_suggestion[index]);
+                } else {
+                  _likedWords.add(_suggestion[index]);
+                }
+              });
+            },
+          );
+        },
+      ),
     );
-    }
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _likedWords.map(
+        (pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        },
+      );
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList()
+          : <Widget>[];
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Likes Names'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
+  }
 }
